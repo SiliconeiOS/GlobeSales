3db60d9b5a3adc9bea31f2680b6b65c7cdec0c45
SiliconeiOS <johnsmithjunior28122000@gmail.com>
2025-07-07 15:43:54 +0300

    Initial Commit

---
+++ GlobeSales/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlobeSales
+//
+//  Created by Иван Дроботов on 7/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
